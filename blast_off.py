3090af1d5933717487f90c3235e4bca41562bafa
Potatoqzx <nathaniell95@nycstudents.net>
2025-11-07 12:58:25 -0500

    upload project

---
+++ blast_off.py
@@ -0,0 +1,11 @@
+from functions import*
+from random import*
+turtle.colormode(255)
+turtle.tracer(False)
+turtle.bgcolor(200,100,200)
+spiral2(400,8)
+jerry.width(10)
+jump(0,0)
+spiral3(400,8)
+jump(0,0)
+spiral(400,2)
